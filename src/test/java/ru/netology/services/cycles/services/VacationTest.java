7e67fbd0996814623d86558f17208f25363b5988
Vladimir Efanov <vuefanov@yandex.ru>
2023-06-20 14:55:53 +0300

    added csv file

--- src/test/java/ru/netology/services/cycles/services/VacationTest.java
+++ src/test/java/ru/netology/services/cycles/services/VacationTest.java
@@ -2,31 +2,38 @@ package ru.netology.services.cycles.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class VacationTest {
 
-    @Test
-    public void VacationTest() /*(int expected, int income, int expenses, int threshold)*/ {
+    @ParameterizedTest
+/*    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    }) */
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void VacationTest(int expected, int income, int expenses, int threshold) {
         Vacation service = new Vacation();
-        int expected = 3;
+        /*int expected = 3;
         int income = 10_000; // доход от работы
         int expenses = 3_000; // обязательные месячные траты
-        int threshold = 20_000; // есть возможность отдохнуть
+        int threshold = 20_000; // есть возможность отдохнуть */
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-      @Test
-      public void VacationTest1() /*(int expected, int income, int expenses, int threshold)*/ {
-      Vacation service = new Vacation();
-      int expected = 2;
-      int income = 100_000; // доход от работы
-      int expenses = 60_000; // обязательные месячные траты
-      int threshold = 150_000; // есть возможность отдохнуть
-      int actual = service.calculate(income, expenses, threshold);
-
+/*    @Test
+    public void VacationTest1() (int expected, int income, int expenses, int threshold) {
+        Vacation service = new Vacation();
+        int expected = 2;
+        int income = 100_000; // доход от работы
+        int expenses = 60_000; // обязательные месячные траты
+        int threshold = 150_000; // есть возможность отдохнуть
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
-    }
+    } */
 
 }
